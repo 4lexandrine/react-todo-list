257bb63c622416bea14c2dfebb9cf18a23580d99
Alexandrine Defoor <alexandrine.defoor@gmail.com>
2020-03-27 11:53:08 +0100

    acorn & minimist vulnerabilities fixed

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import List from "./components/List";
 function App() {
 
   const [todos, setTodos] = useState(""); // état pour créer un todo
-  const [list, setList] = useState([{todo: "⬅ Click here to mark todo as checked", done: false}, {todo: "Click here to delete todo ➡", done: false}]); // état pour afficher un todo
-
+  const [list, setList] = useState([{ todo: "⬅ Click here to mark todo as checked", done: false }, { todo: "Click here to delete todo ➡", done: false }]); // état pour afficher un todo
 
   return (
     <div>
@@ -18,22 +17,20 @@ function App() {
       <div >
         <div className="container">
           <List
-          setTodos={setTodos}
-          todos={todos}
-          list={list}
-          setList={setList}
+            list={list}
+            setList={setList}
           />
-          <Todo 
-          setTodos={setTodos} 
-          todos={todos} 
-          list={list} 
-          setList={setList} />
+          <Todo
+            setTodos={setTodos}
+            todos={todos}
+            list={list}
+            setList={setList} />
         </div>
 
-      <footer>
-        <p>Made with <a href="#">React</a> at <a href="#">Le Reacteur</a> by <a href="#">Me</a>.</p>
-      </footer>
-    </div >
+        <footer>
+          <p>Made with <a href="#!">React</a> at <a href="#!">Le Reacteur</a> by <a href="#!">Me</a>.</p>
+        </footer>
+      </div >
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,18 +5,18 @@ const Todo = ({ todos, setTodos, list, setList }) => {
         <form className="newTask" onSubmit={(e) => {
             e.preventDefault();
             if (todos) {
-            const newList = [...list];
-            newList.push({todo: todos, done: false});
-            setList(newList)
-            setTodos("")
-                }
+                const newList = [...list];
+                newList.push({ todo: todos, done: false });
+                setList(newList)
+                setTodos("")
+            }
         }} >
-            {/* je crée mon input qui aura pour valeur, ce que l'utilisateur écrit  */}
+            {/* je crée mon input qui aura pour valeur ce que l'utilisateur écrit  */}
             <input className="newTask" type="text" value={todos} placeholder="add a new task" onChange={(event) => {
                 setTodos(event.target.value)
 
             }} />
-            {/* quand je clic sur le bouton add, je mets à jour ma liste de todos à afficher */}
+            {/* quand je clic sur le bouton add, je mets à jour ma liste de todos à afficher via la méthode onSubmit du formulaire */}
             <button type="submit" >Add</button>
 
         </form>
